d5a872babe98bad4d20879d36ea57a1dcd77bdfc
Alireza Arabi <alirezaaraby5@gmail.com>
2024-06-07 04:00:21 -0700

    permissions completed

---
+++ users/context_processors.py
@@ -0,0 +1,21 @@
+from .models import Permissions
+from django.shortcuts import get_object_or_404
+
+def add_permissions(request):
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            permissions = {
+                'daily_evaluation': True,
+                'personnel': True,
+                'groups': True,
+                'indicators': True,
+                'substitute': True,
+                'logs': True,
+                'reports': True,
+            }
+        else:
+            permissions = get_object_or_404(Permissions, pid=request.user)
+    else:
+        permissions = None
+        
+    return {'menu_permissions': permissions}
