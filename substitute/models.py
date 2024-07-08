addaa5eddfe69622040461e7bcd2f11843f91ec9
Alireza Arabi <alirezaaraby5@gmail.com>
2024-07-08 03:33:34 -0700

    substitue added

--- substitute/models.py
+++ substitute/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from users.models import users
 # Create your models here.
+
+class Substitute(models.Model):
+    pid = models.ForeignKey(users, on_delete=models.CASCADE, related_name='users_as_pid')
+    substitute_id = models.ForeignKey(users, on_delete=models.CASCADE, related_name='users_as_substitute')
